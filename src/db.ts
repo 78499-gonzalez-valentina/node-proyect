c71c616cdc49c022c8d13d7654d1e7ceea39643e
vvalugonzalez <valu322@gmail.com>
2024-11-15 09:32:39 -0300

    variables de entorno

--- src/db.ts
+++ src/db.ts
@@ -6,16 +6,23 @@ import { EstadoEntity } from "./entity/EstadoEntity";
 import { ProyectoEntity } from "./entity/ProyectoEntity";
 import { TareaEntity } from "./entity/TareaEntity";
 
+require('dotenv').config({ path: './.env' });
+
 export const dataSource = new DataSource({
   type:'postgres',
-  host:'strikingly-cool-mullet.data-1.use1.tembo.io',
+  host: 'strikingly-cool-mullet.data-1.use1.tembo.io',
   port:5432,
-  username: 'postgres',
-  password: 'tnxstXwXzubch6tZ',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: 'postgres',
   entities: [DesarrolladorEntity, RolEntity, DesarrolladorXProyectoEntity, EstadoEntity, ProyectoEntity, TareaEntity],
   logging:true,
   ssl:{
     rejectUnauthorized: false, // To allow SSL connections to the database
   }
+
+  
 })
+
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_username:', process.env.DB_USERNAME);
